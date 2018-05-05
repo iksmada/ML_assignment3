ff74b12665377b5b7794cdd459499101fa609a55
Raphael Adamski <raphaeladamski@hotmail.com>
2018-05-05 16:09:44 -0300

    add plot, fix cluster code

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import csv
 import numpy as np
 
 from sklearn import cluster, model_selection, metrics
+import matplotlib.pyplot as plt
 
 from joblib import Parallel, delayed
 import multiprocessing
@@ -31,8 +32,9 @@ dates = []
 headlines = []
 with open('news_headlines.csv') as csvfile:
     rows = csv.reader(csvfile)
-    #jump header
-    for row in rows[1:0]:
+    # jump header
+    next(rows, None)
+    for row in rows:
         dates.append(row[0])
         headlines.append(row[1])
 
@@ -45,13 +47,19 @@ trainSamples = Parallel(n_jobs=num_cores)(
 #X_train, X_test = model_selection.train_test_split(trainSamples, train_size=0.8)
 
 scores = []
-for n in range(1,10):
+clusters = range(1, 10)
+for n in clusters:
     # Mini batch is faster
-    # cluster = cluster.KMeans(n_clusters=n, n_jobs=-1)
-    cluster = cluster.MiniBatchKMeans(n_clusters=n)
+    # model = cluster.KMeans(n_clusters=n, n_jobs=-1)
+    model = cluster.MiniBatchKMeans(n_clusters=n)
 
-    cluster.fit(trainSamples)
+    model.fit(trainSamples)
     # could score test set
-    scores.append(cluster.score(trainSamples))
+    scores.append(model.score(trainSamples))
+
+# plot
+plt.scatter(clusters, scores)
+plt.plot(clusters, scores)
+plt.show()
 
 # print(metrics.accuracy_score(y_test, pred))
